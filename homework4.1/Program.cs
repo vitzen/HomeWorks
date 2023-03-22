6a843b638b7ec75494b140141b77de27aa100523
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-22 20:21:47 +0300

    # changes

--- homework4.1/Program.cs
+++ homework4.1/Program.cs
@@ -1,13 +1,11 @@
 ﻿// Домашнее задание
 //1. Найти сумму элементов одномерного массива. Размер произвольный. Элементы вводятся с клавиатуры.
 
-using System;
-
 namespace homework4._1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //Ввод с консоли
 
