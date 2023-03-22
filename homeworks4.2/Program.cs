2c4861ab3c0c46bcda9780fbc52d002e973e72a8
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-22 14:02:29 +0300

    # create homework 4.2

---
+++ homeworks4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Домашнее задание
+//1. Найти сумму элементов одномерного массива. Размер произвольный. Элементы вводятся с клавиатуры.
+
+using System;
+
+namespace homework4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            
+            
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
