89518059a3f6befda24808a7e3d664faa573f0ba
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-22 19:02:50 +0300

    # project 4.2 finished

--- homework4.2/Program.cs
+++ homework4.2/Program.cs
@@ -13,30 +13,31 @@ namespace homework4._2
             Console.WriteLine("Введите размерность (количество элементов) массива");
             string inputFromConsole = Console.ReadLine();
             bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
-            
+
             //Блок инициализации массива и заполнения массива числами
             if (consoleInput)
             {
                 int[] myArray = new int[arrayElementsCount];
                 for (int i = 0; i < myArray.Length; i++)
                 {
-                  Console.WriteLine("Введите число");
-                  string inputNumberFromConsole = Console.ReadLine();
-                  bool number = Int32.TryParse(inputNumberFromConsole, out int numberOfArray);
-                  if (number)
-                  {
-                      myArray[i] = numberOfArray; //Записываем в массив наши числа
-                  }
-                  else
-                  {
-                      Console.WriteLine("Некорректно введенное число, пробуйте заново!!!");
-                  }
+                    Console.WriteLine("Введите число");
+                    string inputNumberFromConsole = Console.ReadLine();
+                    bool number = Int32.TryParse(inputNumberFromConsole, out int numberOfArray);
+                    if (number)
+                    {
+                        myArray[i] = numberOfArray; //Записываем в массив наши числа
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректно введенное число, пробуйте заново!!!");
+                    }
                 }
 
-                Console.WriteLine($"Элементы вашего массива: {string.Join(" ", myArray)}");
-                double result = (myArray.Sum())/;
-
-
+                Console.WriteLine(
+                    $"Элементы вашего массива: {string.Join(" ", myArray)}"); //Выводим на консоль элементы массива
+                double
+                    result = (myArray.Sum() / arrayElementsCount); //Подсчет среднего-арифметического значения среди элементов массива
+                Console.WriteLine($"Среднее арифметическое значение всех элументов нащего массива = {result}");
             }
             else
             {
