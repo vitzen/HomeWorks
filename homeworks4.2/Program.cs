5769c307b1994c1592a9aca77f664d405f56c00e
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-22 14:26:14 +0300

    # changes in project 4.2

--- homeworks4.2/Program.cs
+++ homeworks4.2/Program.cs
@@ -1,20 +1,48 @@
 ﻿// Домашнее задание
-//1. Найти сумму элементов одномерного массива. Размер произвольный. Элементы вводятся с клавиатуры.
+//2. Задан массив А, состоящий из n чисел. Найти среднее арифметическое его элементов. Элементы вводятся с клавиатуры.
 
 using System;
 
-namespace homework4._1
+namespace homework4._2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Воод размерности массива
+            Console.WriteLine("Введите размерность (количество элементов) массива");
+            string inputFromConsole = Console.ReadLine();
+            bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
             
-            
-            
-            
-            
-            
+            //Блок инициализации массива и заполнения массива числами
+            if (consoleInput)
+            {
+                int[] myArray = new int[arrayElementsCount];
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                  Console.WriteLine("Введите число");
+                  string inputNumberFromConsole = Console.ReadLine();
+                  bool number = Int32.TryParse(inputNumberFromConsole, out int numberOfArray);
+                  if (number)
+                  {
+                      myArray[i] = numberOfArray; //Записываем в массив наши числа
+                  }
+                  else
+                  {
+                      Console.WriteLine("Некорректно введенное число, пробуйте заново!!!");
+                  }
+                }
+
+                Console.WriteLine($"Элементы вашего массива: {string.Join(" ", myArray)}");
+                double result = (myArray.Sum())/;
+
+
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введенное число, Запускаем программу заново");
+            }
+
 
             // Delay
             Console.ReadKey();
