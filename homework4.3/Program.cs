c255a9b1d19b343b394d05d1a3ab47481994173e
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-23 14:56:12 +0300

    # changes

--- homework4.3/Program.cs
+++ homework4.3/Program.cs
@@ -14,10 +14,14 @@ namespace homework4._3
             string inputFromConsole = Console.ReadLine();
             bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
 
+
             //Блок инициализации массива и заполнения массива числами
             if (consoleInput)
             {
                 int[] myArray = new int[arrayElementsCount];
+                // int minNumber = myArray[0]; // Инициализация новой переменой для блока нахождения min значения в массиве
+
+
                 for (int i = 0; i < myArray.Length; i++)
                 {
                     Console.WriteLine("Введите число");
@@ -35,26 +39,18 @@ namespace homework4._3
 
                 Console.WriteLine(
                     $"Элементы вашего массива: {string.Join(" | ", myArray)}"); //Выводим на консоль элементы массива
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+
                 // Реализация сортировки массива и нахождения наименьшего элемента
-                // int[] newArray = new int[arrayElementsCount];
-                // newArray = myArray;
-                // Console.WriteLine($"Элементы нового массива: {string.Join(" | ", newArray)}");
+                int minNumber = myArray[0];
+                for (int i = 1; i < myArray.Length; i++)
+                {
+                    if (myArray[i] < minNumber)
+                    {
+                        minNumber = myArray[i];
+                    }
+                }
 
-
-
-
+                Console.WriteLine($"Минимальный элемент в массиве равен: {minNumber}");
             }
             else
             {
