b59697387643e60ba9e0d68497660f84c7064468
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-25 19:52:25 +0300

    # changes in project 4.5 # add new Array

--- homework4.5/Program.cs
+++ homework4.5/Program.cs
@@ -9,50 +9,51 @@ namespace homework4._5
     {
         static void Main()
         {
+            //Блок ввода количества элементов массива
             Console.WriteLine("Введите количество элементов массива");
             string inputNumber = Console.ReadLine();
-            bool num = Int32.TryParse(inputNumber, out int array1_CounterNumber);
-            int array2_CounterNumber = 0; // Инициализируем по умолчанию размерность нового массива
+            bool num = Int32.TryParse(inputNumber, out int array_CounterNumber);
+
+
+            //Блок инициализаторов массивов
+            int[] myArray = new int[array_CounterNumber];
+            int[] newArray = new int[array_CounterNumber];
+            
 
             if (num)
             {
-                int[] myArray = new int[array1_CounterNumber]; //Инициализация массива 1
-                int[] signArray = new int[array2_CounterNumber];
-
                 //Блок ввода элементов массива
-                for (int i = 0; i < myArray.Length; i++)
+                for (int i = 0; i < array_CounterNumber; i++)
                 {
                     Console.WriteLine("Введите элемент массива");
                     string inputElement = Console.ReadLine();
                     bool element = Int32.TryParse(inputElement, out int arrayElement);
 
-
                     if (element)
                     {
                         myArray[i] = arrayElement;
-                        if (arrayElement >= 0)
-                        {
-                            array2_CounterNumber++;
-                            //signArray[i] = arrayElement;
-                        }
                     }
-
                     else
                     {
                         Console.WriteLine("Введите корректное число в качестве элемента массива");
                     }
                 }
-
                 //Блок вывода элементов получившегося массива
                 Console.WriteLine($"Элементы вашего массива: {String.Join(" | ", myArray)}");
-                //Console.WriteLine($"Элементы нового массива: {String.Join(" | ", signArray)}");
+                
+                int indexOfNewArray = 0;
+                for (int j= 0; j < newArray.Length; j++)
+                {
+                    int currentNumber = newArray[j];
+                    if (currentNumber > 0)
+                    {
+                        newArray[indexOfNewArray++] = currentNumber;
+                    }
+                }
+                //Блок вывода элементов получившегося массива
+                Console.WriteLine($"Элементы нового массива: {String.Join(" | ", newArray)}");
 
-                //Блок инициализации нового массива и операции над элементами массива
-                // int[] signArray;
-                // signArray = myArray.Where();
-
-
-                //Console.WriteLine($"Элементы нового массива: {String.Join(" | ", signArray)}");
+                
             }
             else
             {
