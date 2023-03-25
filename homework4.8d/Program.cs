000290037baeb7f85be905093ef6ec8e69ba3d9d
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-26 00:08:54 +0300

    # create project 4.8d

---
+++ homework4.8d/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задание с сайта https://pas1.ru/taskcycle
+//В
+
+using System;
+
+namespace homework4._8d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
