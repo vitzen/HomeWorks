7d377da2c63e71e96411f337234d21b82f6ab82f
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-27 20:04:47 +0300

    # create project 5.1 # add CalculateMethod

---
+++ homework5.1/Program.cs
@@ -0,0 +1,62 @@
+﻿// Домашнее задание (ПЕРЕДЕЛАТЬ МАССИВЫ НА МЕТОДЫ)
+//1. Найти сумму элементов одномерного массива. Размер произвольный. Элементы вводятся с клавиатуры.
+
+namespace homework5._1
+{
+    class Program
+    {
+        
+        static void CalculateMethod()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        static void Main()
+        {
+            //Ввод с консоли
+
+            Console.WriteLine("Введите размерность (количество элементов) массива");
+            string inputFromConsole = Console.ReadLine();
+            bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
+
+            if (consoleInput)
+            {
+                int[] myArray = new int[arrayElementsCount];
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.WriteLine("Введите число");
+                    string number = Console.ReadLine();
+                    bool numberInput = Int32.TryParse(number, out int num);
+                    if (numberInput)
+                    {
+                        myArray[i] = num;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректно введенное число, Запускаем программу заново");
+                    }
+                }
+
+                // Полученный после ввода массив данных
+                Console.WriteLine($"Элементы вашего массива: {string.Join(" ", myArray)}");
+                int sumOfArrayElements = myArray.Sum();
+                Console.WriteLine($"Сумма всех элементов в массиве равна = {sumOfArrayElements} ");
+            }
+
+            else
+            {
+                Console.WriteLine("Введите корректное число элементов в вашем массиве");
+            }
+
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
