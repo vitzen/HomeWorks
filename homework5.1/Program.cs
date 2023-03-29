f987e0fa861784319503a53a94ef0ae771015198
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-03-29 17:31:42 +0300

    # changes

--- homework5.1/Program.cs
+++ homework5.1/Program.cs
@@ -5,17 +5,18 @@ namespace homework5._1
 {
     class Program
     {
-        static int[] CalculateMethod(string inputFromConsole)
+        static (int[], int sumOfArrayElements)  CalculateMethod(string consoleInput)
         {
-            bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
+            //int sumOfArrayElements = 0;
+            bool consoleInputNumber = Int32.TryParse(consoleInput, out int arrayElementsCount);
             int[] myArray = new int[arrayElementsCount];
-            if (consoleInput)
+            if (consoleInputNumber)
+
             {
                 for (int i = 0; i < myArray.Length; i++)
                 {
-                    Console.WriteLine("Введите число");
-                    string number = Console.ReadLine();
-                    bool numberInput = Int32.TryParse(number, out int num);
+                    string numberInp = InputArrayNumberMethod();
+                    bool numberInput = Int32.TryParse(numberInp, out int num);
                     if (numberInput)
                     {
                         myArray[i] = num;
@@ -26,11 +27,8 @@ namespace homework5._1
                     }
                 }
 
-                // Полученный после ввода массив данных
-
-                // int sumOfArrayElements = myArray.Sum();
-                // Console.WriteLine($"Элементы вашего массива: {string.Join(" ", myArray)}");
-                // Console.WriteLine($"Сумма всех элементов в массиве равна = {sumOfArrayElements} ");
+                int sumOfArrayElements = myArray.Sum();
+                
             }
 
             else
@@ -38,18 +36,33 @@ namespace homework5._1
                 Console.WriteLine("Введите корректное число элементов в вашем массиве");
             }
 
-            return myArray;
+            return (myArray, sumOfArrayElements);
         }
+
+        ///Метод ввода размерности массива
+        static void InputArraySizeMethod()
+        {
+            Console.WriteLine("Введите размерность (количество элементов) массива");
+            string inputSymbol = Console.ReadLine();
+            CalculateMethod(inputSymbol);
+        }
+
+        ///Метод ввода чисел массива
+        static string InputArrayNumberMethod()
+        {
+            Console.WriteLine("Введите число");
+            string inputNumber = Console.ReadLine();
+            return inputNumber;
+        }
+
 
         static void Main()
         {
-            //Ввод с консоли
+            Console.WriteLine("Программа для подсчета суммы элементов в массиве");
+            InputArraySizeMethod();
 
-            Console.WriteLine("Введите размерность (количество элементов) массива");
-            string inp = Console.ReadLine();
-            CalculateMethod(string inp);
-
-            
+            // Console.WriteLine($"Элементы вашего массива: {string.Join(" ", myArray)}");
+            // Console.WriteLine($"Сумма всех элементов в массиве равна = {sumOfArrayElements} ");
 
 
             // Delay
