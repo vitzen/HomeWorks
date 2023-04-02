54fb71ed6fe04c60a3f7fb4eef2f930fad8387d9
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-04-02 19:56:09 +0300

    #changes

--- homework6.1/Program.cs
+++ homework6.1/Program.cs
@@ -1,2 +1,47 @@
 ﻿//Домашнее заданий 6.1
 //Написать метод-рекурсию для вычисления N-ого элемента последовтельности Фиббоначчи
+
+using System;
+
+namespace homework6._1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine("Программа для вычисления N-ого элемента последовтельности Фиббоначчи, используя метод-рекурсию ");
+            int number;
+            result = 0;
+            int Recursion(number)
+            {
+                if (number == 0)
+                {
+                    return 1;
+                }
+
+                else
+                {
+                    result = Recursion((number-1)*number));
+                }
+                
+                
+              
+            }
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
