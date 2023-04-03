a43620cf6be430d9f35ea3c8761aef07ad39af0a
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-04-03 15:42:43 +0300

    #project 6.1 finished

--- homework6.1/Program.cs
+++ homework6.1/Program.cs
@@ -7,34 +7,55 @@ namespace homework6._1
 {
     class Program
     {
-        public static int Fibonachi(int n) //Метод для вычисления n-числа Фибоначчи
+        ///
+        /// <summary>
+        /// Метод, осуществляющий парсинг символа от пользователя
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static void Parsing(string inputFromUser)
+        {
+            string unknownInput = inputFromUser;
+            bool input = Int32.TryParse(unknownInput, out int nElement);
+            if (input)
+            {
+                Print(Fibonachi(nElement));;
+            }
+            else
+            {
+                Console.WriteLine("Введите корректное число");
+            }      
+        }
+        
+        /// <summary>
+        /// Алгоритм поиска n-т-ого элемента в ряду Фибоначчи
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Fibonachi(int n)
         {
             if (n == 0 || n == 1) return n;
             return Fibonachi(n - 1) + Fibonachi(n - 2);
         }
 
+        /// <summary>
+        /// Метод для вывода на консоль
+        /// </summary>
+        /// <param name="fibonachResult"></param>
+        public static void Print(int fibonachResult)
+        {
+            Console.WriteLine($"n-ое число Фибоначчи = {fibonachResult}");
+        }
 
-        public static void Main() // Программа
+        public static void Main()
         {
             Console.WriteLine(
                 "Программа для вычисления N-ого элемента последовтельности Фиббоначчи, используя метод-рекурсию \n" +
                 "Введите n-ый элемент для вычисления его в ряду Фибоначчи");
             string inputSymbol = Console.ReadLine();
-            bool input = Int32.TryParse(inputSymbol, out int nElement);
-            //int result = 0;
-            if (input == true && nElement >= 0)
-            {
-                int result = Fibonachi(nElement);
-            }
-            else
-            {
-                Console.WriteLine("Введите число корректно");
-            }
-
-
-            //Console.WriteLine($"n-ое число Фибоначчи = {result}");
-
-
+            Parsing(inputSymbol);
+            
+            
             // Delay
             Console.ReadKey();
         }
