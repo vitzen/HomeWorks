f0be41db04c44d56792dc26f4c58acd9ea16732a
Vitalii Zenkov <zenkov.dev@gmail.com>
2023-04-08 00:26:57 +0300

    #changes

--- homework6.3/Car.cs
+++ homework6.3/Car.cs
@@ -1,6 +1,57 @@
+using System.Drawing;
+
 namespace homework6._3;
 
-public class Car
+public class Car //Класс автомобиль
 {
-    int 
+    public enum CarColorState
+    {
+        White,
+        Metallic,
+        Black,
+        Blue,
+        Yellow,
+        Green,
+        Red
+    }
+
+    private string _manufactureName; //Название/марка автомобиля
+    private int _manufactureYear; //Год выпуска
+    private int _enginePower; //Мощность двигателя
+    private double _carMileage; //Пробег
+    private CarColorState _color; //Цвет автомобиля
+
+    
+
+    /// <summary>
+    /// Получить пробег
+    /// </summary>
+    /// <returns></returns>
+    public double GetMilage()
+    {
+        return _carMileage;
+    }
+
+    /// <summary>
+    /// Получить пробег через 100км
+    /// </summary>
+    public void Go_100_Mile()
+    {
+        _carMileage += 100;
+    }
+
+    /// <summary>
+    /// Получить возраст автомобиля на данный момент времени
+    /// </summary>
+    /// <returns></returns>
+    public int CarAgeYear()
+    {
+        return (DateTime.Now.Year - _manufactureYear);
+    }
+
+    public override string ToString()
+    {
+        return
+            $"CarColorState: {_color}, ManufactureName: {_manufactureName}, ManufactureYear: {_manufactureYear}, EnginePower: {_enginePower}, CarMileage: {_carMileage}, CarAge: {_carAge}";
+    }
 }
--- homework6.3/Program.cs
+++ homework6.3/Program.cs
@@ -7,12 +7,9 @@ namespace homework6._3
 {
     class Program
     {
-
-
         public static void Main()
         {
-            
-
+            Car peugeot = new Car(Car.CarColorState.Blue, "Honda Civic", 2005, 100, 100);
 
 
             // Delay
