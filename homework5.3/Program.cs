bb05307287f994c94c46484ed2b68df3196fe19f
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-04-18 16:15:31 +0300

    #project 5.3 finished

--- homework5.3/Program.cs
+++ homework5.3/Program.cs
@@ -3,62 +3,80 @@
 
 using System;
 
-namespace homework4._3
+namespace homework5._3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /// Метод парсинга размера массива
+            static int InputArrayCountMethod(string inputFromConsole)
+            {
+                bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
+                if (!consoleInput)
+                {
+                    Console.WriteLine($"Введите корректное значение");
+                }
+
+                return arrayElementsCount;
+            }
+
+            /// Метод парсинга вводимых значений эелемнтов массива
+            static int InputArrayElementsMethod(string number)
+            {
+                bool numberInput = Int32.TryParse(number, out int num);
+                if (!numberInput)
+                {
+                    Console.WriteLine($"Введите корректное значение");
+                }
+
+                return num;
+            }
+
+            /// Метод для решения арифметических задач
+            static int CalculateMethod(int[] newArray)
+            {
+                int minNumber = newArray[0];
+                for (int i = 1; i < newArray.Length; i++)
+                {
+                    if (newArray[i] < minNumber)
+                    {
+                        minNumber = newArray[i];
+                    }
+                }
+
+                return minNumber;
+            }
+
+            /// Метод для вывода решения
+            static void PrintResultMethod(int[] newArray, int result)
+            {
+                Console.WriteLine($"Элементы вашего массива: {string.Join(" ", newArray)}");
+                Console.WriteLine($"Наименьший элемент в массиве равен = {result} ");
+            }
+
+
             //Воод размерности массива
             Console.WriteLine("Введите размерность (количество элементов) массива");
             string inputFromConsole = Console.ReadLine();
-            bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
+            int inputElementsCount = InputArrayCountMethod(inputFromConsole);
 
 
-            //Блок инициализации массива и заполнения массива числами
-            if (consoleInput)
+            int[] array = new int[inputElementsCount];
+
+
+            for (int i = 0; i < array.Length; i++)
             {
-                int[] myArray = new int[arrayElementsCount];
-                // int minNumber = myArray[0]; // Инициализация новой переменой для блока нахождения min значения в массиве
+                Console.WriteLine("Введите число");
+                string inputNumberFromConsole = Console.ReadLine();
+                int element = InputArrayElementsMethod(inputNumberFromConsole);
 
-
-                for (int i = 0; i < myArray.Length; i++)
-                {
-                    Console.WriteLine("Введите число");
-                    string inputNumberFromConsole = Console.ReadLine();
-                    bool number = Int32.TryParse(inputNumberFromConsole, out int numberOfArray);
-                    if (number)
-                    {
-                        myArray[i] = numberOfArray; //Записываем в массив наши числа
-                    }
-                    else
-                    {
-                        Console.WriteLine("Некорректно введенное число, пробуйте заново!!!");
-                    }
-                }
-
-                Console.WriteLine(
-                    $"Элементы вашего массива: {string.Join(" | ", myArray)}"); //Выводим на консоль элементы массива
-
-                // Реализация сортировки массива и нахождения наименьшего элемента
-                int minNumber = myArray[0];
-                for (int i = 1; i < myArray.Length; i++)
-                {
-                    if (myArray[i] < minNumber)
-                    {
-                        minNumber = myArray[i];
-                    }
-                }
-
-                Console.WriteLine($"Минимальный элемент в массиве равен: {minNumber}");
-            }
-            else
-            {
-                Console.WriteLine("Некорректно введенное число, Запускаем программу заново");
+                array[i] = element; //Записываем в массив наши числа
             }
 
+            int result = CalculateMethod(array);
+            PrintResultMethod(array, result);
 
-            // Delay
             Console.ReadKey();
         }
     }
