e5ca4d02df91a843e025a57c891f67e221c281d5
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-04-19 18:28:15 +0300

    #changes

--- homework5.5/Program.cs
+++ homework5.5/Program.cs
@@ -2,13 +2,37 @@
 // 5. Массив А вводится с клавиатуры. Сформировать новый массив В, состоящий из положительных элементов массива А. Размер произвольный.
 
 using System;
-?????????????????????????????????????????????????????????????????????????????????????????????????
 namespace homework5._5
 {
     class Program
     {
         static void Main()
         {
+            //начало программы
+            Console.WriteLine(
+                "Программа для формирования нового массива В, состоящий из положительных элементов массива А\n" +
+                "Введите количество элементов массива");
+            string inputNumber = Console.ReadLine();
+            int consoleInput = InputArrayCountMethod(inputNumber);
+
+            int newArrayCounterNumber = consoleInput;
+            int[] array_A = new int[consoleInput];
+            int[] array_B = new int[newArrayCounterNumber];
+
+
+            for (int i = 0; i < array_A.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива");
+                string inputElement = Console.ReadLine();
+                int element = InputArrayElementsMethod(inputElement);
+
+                array_A[i] = element;
+            }
+
+            int[] array_C = CalculateMethod(array_A, newArrayCounterNumber);
+
+            PrintResultMethod(array_C);
+            
             /// Метод парсинга размера массива
             static int InputArrayCountMethod(string inputFromConsole)
             {
@@ -36,6 +60,7 @@ namespace homework5._5
             /// Метод для решения арифметических задач
             static int[] CalculateMethod(int[] array_B, int newArrayCounterNumber)
             {
+                //int[] array_B = new int[newArrayCounterNumber];
                 int indexOfNewArray = 0;
                 for (int i = 0; i < newArrayCounterNumber; i++)
                 {
@@ -50,37 +75,12 @@ namespace homework5._5
             }
 
             /// Метод для вывода решения
-            static void PrintResultMethod(int[] array_C)
+            static void PrintResultMethod(int[] array_D)
             {
-                Console.WriteLine($"Элементы нового массива: {string.Join(" ", array_C)}");
+                Console.WriteLine($"Элементы нового массива: {string.Join(" ", array_D)}");
                 
             }
 
-
-            //Блок ввода количества элементов массива
-            Console.WriteLine(
-                "Программа для формирования нового массива В, состоящий из положительных элементов массива А\n" +
-                "Введите количество элементов массива");
-            string inputNumber = Console.ReadLine();
-            int consoleInput = InputArrayCountMethod(inputNumber);
-
-            int newArrayCounterNumber = consoleInput;
-            int[] array_A = new int[consoleInput];
-            int[] array_B = new int[newArrayCounterNumber];
-
-
-            for (int i = 0; i < array_A.Length; i++)
-            {
-                Console.WriteLine("Введите элемент массива");
-                string inputElement = Console.ReadLine();
-                int element = InputArrayElementsMethod(inputElement);
-
-                array_B[i] = element;
-            }
-
-            int[] array_C = CalculateMethod(array_B, newArrayCounterNumber);
-
-            PrintResultMethod(array_C);
 
             
 
--- homework5.6d/Program.cs
+++ homework5.6d/Program.cs
@@ -10,7 +10,6 @@ namespace homework5._6d
     {
         static void Main(string[] args)
         {
-            
             /// Метод парсинга размера массива
             static int InputArrayCountMethod(string inputFromConsole)
             {
@@ -55,60 +54,58 @@ namespace homework5._6d
             {
                 Console.WriteLine($"Элементы вашего массива: {string.Join(" ", newArray)}");
                 Console.WriteLine($"Сумма четных лементов массива равна = {finishResult} ");
-            } 
-            
-            
-            
-           
-            
+            }
+
+
             // Блок ввода размерности массива
             Console.WriteLine("Программа для подсчета количества простых чисел \n" +
                               "Введите количество чисел для подсчета суммы:");
             string inputConsoleCounter = Console.ReadLine();
-            bool consoleCounter = Int32.TryParse(inputConsoleCounter, out int inputArrayCounter);
-            if (consoleCounter)
+            int consoleInput = InputArrayCountMethod(inputConsoleCounter);
+
+
+            int[] inputArray = new int[inputArrayCounter];
+            int sum = 0;
+
+            // Блок ввода элементов массива
+            for (int i = 0; i < inputArrayCounter; i++)
             {
-                // Блок инициализации массива
-                int[] inputArray = new int[inputArrayCounter];
-                int sum = 0;
+                Console.WriteLine("Введите число");
+                string inputConsoleNumber = Console.ReadLine();
+                bool number = Int32.TryParse(inputConsoleNumber, out int inputNumber);
 
-                // Блок ввода элементов массива
-                for (int i = 0; i < inputArrayCounter; i++)
+                if (number)
                 {
-                    Console.WriteLine("Введите число");
-                    string inputConsoleNumber = Console.ReadLine();
-                    bool number = Int32.TryParse(inputConsoleNumber, out int inputNumber);
-
-                    if (number)
+                    inputArray[i] = inputNumber;
+                    if ((inputNumber > 0) && (inputNumber / inputNumber == 1) && (inputNumber / 1 == inputNumber))
                     {
-                        inputArray[i] = inputNumber;
-                        if ((inputNumber > 0) && (inputNumber / inputNumber == 1) && (inputNumber / 1 == inputNumber))
-                        {
-                            sum += inputNumber;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Простые числа не могут быть отрицательными или равны 0, повторите ввод заново");
-                        }
+                        sum += inputNumber;
                     }
                     else
                     {
-                        Console.WriteLine("Введите корректное число для работы программы");
+                        Console.WriteLine(
+                            "Простые числа не могут быть отрицательными или равны 0, повторите ввод заново");
                     }
                 }
-
-                Console.WriteLine($"Введенные вами числа для подсчета : {String.Join(" | ", inputArray)}");
-                Console.WriteLine($"Сумма простых чисел равна : {sum}");
+                else
+                {
+                    Console.WriteLine("Введите корректное число для работы программы");
+                }
             }
 
-            else
-            {
-                Console.WriteLine("Введите корректное число");
-            }
-
-
-            // Delay
-            Console.ReadKey();
+            Console.WriteLine($"Введенные вами числа для подсчета : {String.Join(" | ", inputArray)}");
+            Console.WriteLine($"Сумма простых чисел равна : {sum}");
         }
+        else
+
+        {
+            Console.WriteLine("Введите корректное число");
+        }
+
+
+        // Delay
+        Console.ReadKey();
     }
+}
+
 }
