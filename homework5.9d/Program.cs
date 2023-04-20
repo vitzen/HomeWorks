55f1463af04cfdf6b7413c1f69594b3b81d221c3
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-04-20 21:09:08 +0300

    #changes

--- homework5.9d/Program.cs
+++ homework5.9d/Program.cs
@@ -1,26 +1,45 @@
 ﻿//Задание с сайта https://pas1.ru/taskcycle
 //Используя 2 цикла вывести таблицу умножения
 // Домашнее задание (ПЕРЕДЕЛАТЬ ЗАДАНИЕ НА МЕТОДЫ)
+
 using System;
 
 namespace homework4._9d
 {
     class Program
     {
-        static void Main(string[] args)
+        /// Метод для решения арифметических задач
+        static void CalculateMethod()
         {
-            int[,] myarray = new int [10, 10]; //Заполняем массив числами от 0 до 9
-
+            int[,] myArray = new int [10, 10]; //Заполняем массив числами от 0 до 9
+            int number;
             for (int i = 1; i < 10; i++)
             {
                 for (int j = 1; j < 10; j++)
                 {
-                    myarray[i, j] = i * j;
-                    Console.Write($"{myarray[i, j]}\t");
+                    myArray[i, j] = i * j;
+                    number = i * j;
+                    PrintResultMethod(myArray, number);
+                    //Console.Write($"{myArray[i, j]}\t");
                 }
 
                 Console.WriteLine();
             }
+        }
+
+        /// Метод для вывода решения
+        static void PrintResultMethod(int[,] newArray, int num)
+        {
+            //Console.Write($"{newArray}\t");
+            Console.Write($"{string.Join(" ", num)}\t");
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Программа
+            Console.WriteLine("Таблица умножения полученная с помощью двухмерного массива");
+            CalculateMethod();
 
 
             // Delay
