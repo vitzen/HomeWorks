75dc98a36aa06762682a9c5202496b583a9f6ec0
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-05-02 17:20:43 +0300

    #changes

--- homework6.2/MyMethodsClass.cs
+++ homework6.2/MyMethodsClass.cs
@@ -2,36 +2,68 @@ namespace homework6._2;
 
 public class MyMethodsClass
 {
-    /// Метод парсинга размера массива
-    public int InputArrayCountMethod(string inputFromConsole)
+    // Метод, отвечающий за ввод строки с клавиатуры от пользователя,
+    // и осуществляющий дальнейший парсинг в int число
+    public int InputArrayCountMethod()
     {
-        bool consoleInput = Int32.TryParse(inputFromConsole, out int arrayElementsCount);
-        if (!consoleInput)
+        bool inputFromUser = false;
+        int arrayElementsCount = 0;
+
+        do
         {
-            Console.WriteLine($"Введите корректное значение");
-        }
+            Console.WriteLine("Введите количество чисел");
+            string inputSymbol = Console.ReadLine();
+            bool input = Int32.TryParse(inputSymbol, out int outputElement);
+            if (input == false)
+            {
+                Console.WriteLine("Введенное вами число с клавиатуры неверно, попробуйте заново");
+            }
+            else
+            {
+                arrayElementsCount = outputElement;
+                inputFromUser = true;
+                break;
+            }
+        } while (inputFromUser == false);
 
         return arrayElementsCount;
     }
 
-    /// Метод парсинга вводимых значений эелемнтов массива
-    public int InputArrayElementsMethod(string number)
+    // Метод парсинга вводимых значений элемнтов массива
+    public static int[] InputArrayElementsMethod(int elementsNumber)
     {
-        bool numberInput = Int32.TryParse(number, out int num);
-        if (!numberInput)
+        int[] myArray = new int[elementsNumber];
+        for (int i = 0; i < myArray.Length; i++)
         {
-            Console.WriteLine($"Введите корректное значение");
+            bool inputFromUser = false;
+
+            do
+            {
+                Console.WriteLine("Введите целое число");
+                string inputSymbol = Console.ReadLine();
+                bool input = Int32.TryParse(inputSymbol, out int outputElement);
+                if (input == false)
+                {
+                    Console.WriteLine("Введенное вами число с клавиатуры неверно, попробуйте заново");
+                }
+                else
+                {
+                    myArray[i] = outputElement;
+                    inputFromUser = true;
+                    break;
+                }
+            } while (inputFromUser == false);
         }
 
-        return num;
+        return myArray;
     }
 
-    /// Метод для решения арифметических задач
-    public (double, double) CalculateMethod(double[] newArray)
+    // Метод для решения арифметических задач
+    public (int, int) CalculateMethod(int[] newArray)
     {
-        double[] evenOddArray = newArray;
-        double evenCounter = 0;
-        double oddCounter = 0;
+        int[] evenOddArray = newArray;
+        int evenCounter = 0;
+        int oddCounter = 0;
         for (int i = 0; i < evenOddArray.Length; i++)
         {
             if (evenOddArray[i] > 0 && (evenOddArray[i] % 2 == 0))
@@ -49,7 +81,7 @@ public class MyMethodsClass
     }
 
 
-    /// Метод для вывода решения
+    // Метод для вывода решения
     public void PrintResultMethod(double even, double odd)
     {
         Console.WriteLine($"Четные числа вашего массива: {even}");
--- homework6.2/Program.cs
+++ homework6.2/Program.cs
@@ -9,25 +9,17 @@ namespace homework6._2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             MyMethodsClass myMethodsClass = new MyMethodsClass();
-            //Блок ввода количества элементов массива
-            Console.WriteLine("Программа, подсчитывающая количество четных и нечетных цифр в массиве\n" +
-                              "Введите количество чисел для работы программы");
-            string inputConsoleNumber = Console.ReadLine();
-            int consoleInput = myMethodsClass.InputArrayCountMethod(inputConsoleNumber);
 
-            double[] myArray = new double[consoleInput];
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                Console.WriteLine("Введите число");
-                string inputNumberFromConsole = Console.ReadLine();
-                int number = myMethodsClass.InputArrayElementsMethod(inputNumberFromConsole);
-                myArray[i] = number;
-            }
+            Console.WriteLine("Программа, подсчитывающая количество четных и нечетных чисел в массиве\n");
 
-            (double even, double odd) = myMethodsClass.CalculateMethod(myArray);
+            int consoleInput = myMethodsClass.InputArrayCountMethod();
+            int[] array = MyMethodsClass.InputArrayElementsMethod(consoleInput);
+
+
+            (int even, int odd) = myMethodsClass.CalculateMethod(array);
             myMethodsClass.PrintResultMethod(even, odd);
 
             // Delay
