98216b8797dd47b8da3bbd72112b6ccdcdfdb53b
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-05-14 23:28:25 +0300

    # add methods

---
+++ homework11/PaymentTransactions.cs
@@ -0,0 +1,19 @@
+namespace homework11;
+
+public class PaymentTransactions
+{
+    /// <summary>
+    /// Метод для пополнения карты деньгами
+    /// </summary>
+    /// <param name="???"></param>
+    public void Replenishment()
+    {
+        
+    }
+
+    public void Payment()
+    {
+        
+    }
+    
+}
--- homework11/TransportCard.cs
+++ homework11/TransportCard.cs
@@ -2,5 +2,33 @@ namespace homework11;
 
 public class TransportCard
 {
-    
+    private string _cardName;
+    private decimal _moneyBalance = 0;
+
+    public TransportCard(string cardName, decimal moneyBalance)
+    {
+        _cardName = cardName;
+        _moneyBalance = moneyBalance;
+    }
+
+    public string CardName
+    {
+        get => _cardName;
+        set => _cardName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public decimal MoneyBalance
+    {
+        get => _moneyBalance;
+        set => _moneyBalance = value;
+    }
+
+    public delegate void OperationStatus(string notification);
+
+
+    public void Replenishment(amount)
+    {
+        
+    }
 }
+
