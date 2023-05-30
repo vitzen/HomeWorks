67f2d2718f4f7f11e658f89feef0688adcf263ab
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-05-30 15:34:06 +0300

    add project13.2

--- homework13.2/Program.cs
+++ homework13.2/Program.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Text;
+using static homework13._2.TransportCard;
 
 namespace homework13._2
 {
@@ -10,7 +11,44 @@ namespace homework13._2
     {
         public static void Main()
         {
+            decimal replenishmentAmount = 100M; //Сумма пополнения
+            decimal paymentAmount = 30M; //Сумма оплаты проезда
+
+            Console.WriteLine("Программа - ТРАНСПОРТНАЯ КАРТА\n".AddNewNotification());
+            TransportCard transportCard = new TransportCard("Month bus ticket",
+                0M,
+                30M,
+                0.1M, 30M);
+
+            //Подписки
+            transportCard.ReplenishementEvent += SubscribtionClass.ReplenishmentSubscription;
+            transportCard.PaymentEvent += SubscribtionClass.PaymentSubscription;
             
+            // var extraCashbcak= AddExtraCashback.AddSuperCashback(transportCard, transportCard.MoneyBalance);
+            // transportCard.Replenishment(extraCashbcak);
+            
+            transportCard.Replenishment(replenishmentAmount);
+            transportCard.Payment(paymentAmount);
+            transportCard.Payment(paymentAmount);
+            transportCard.Payment(paymentAmount);
+            transportCard.Payment(paymentAmount);
+            transportCard.Payment(paymentAmount);
+            transportCard.Payment(paymentAmount);
+
+            transportCard.Replenishment(replenishmentAmount);
+            transportCard.Payment(paymentAmount);
+            transportCard.Payment(paymentAmount);
+            
+            transportCard.Replenishment(replenishmentAmount);
+            transportCard.Replenishment(replenishmentAmount);
+            transportCard.Replenishment(replenishmentAmount);
+            transportCard.Replenishment(replenishmentAmount);
+            transportCard.Replenishment(replenishmentAmount);
+
+            transportCard.ReplenishementEvent -= SubscribtionClass.ReplenishmentSubscription;
+            transportCard.PaymentEvent -= SubscribtionClass.PaymentSubscription;
+
+            Console.WriteLine(String.Join(",", transportCard.HistoryOfTransactions));
         }
     }
 }
---
+++ homework13.2/TransportCard.cs
@@ -0,0 +1,55 @@
+namespace homework13._2;
+
+public partial class TransportCard
+{
+    public delegate void ReplenishmentDelegate(decimal replParam,
+        decimal balanceAfterReplenishement, decimal balanceAfterCashbackGift);
+
+    public delegate void PaymentDelegate(decimal payParam, decimal balanceAfterPayment);
+
+    public Predicate<decimal> _possibleToPayPredicate; //Предикат проверки возможности оплаты
+    public Func<decimal, decimal> _calculateCashback; //Func для расчета кэшбека при каждом пополнении карты
+
+    private readonly string _cardName;
+    private decimal _moneyBalance = 0;
+    private static decimal _extraCashback;
+
+    public TransportCard(string cardName, decimal moneyBalance, decimal minBalance, decimal calculateCashback,
+        decimal extraCashback)
+    {
+        _cardName = cardName;
+        _moneyBalance = moneyBalance;
+        _extraCashback = extraCashback;
+        _possibleToPayPredicate = (balance) => balance > minBalance;
+        _calculateCashback = (balance) => { return balance *= calculateCashback; };
+    }
+
+    public string CardName
+    {
+        get => _cardName;
+    }
+
+    public decimal MoneyBalance
+    {
+        get => _moneyBalance;
+        set => _moneyBalance = value;
+    }
+
+    public static decimal ExtraCashback
+    {
+        get => _extraCashback;
+        set => _extraCashback = value;
+    }
+
+    public static List<decimal> historyOfTransactions = new List<decimal>(); //Создаем историю платежей
+
+    public List<decimal> HistoryOfTransactions
+    {
+        get => historyOfTransactions;
+        set => historyOfTransactions = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    //Создаем события
+    public event ReplenishmentDelegate? ReplenishementEvent;
+    public event PaymentDelegate? PaymentEvent;
+}
