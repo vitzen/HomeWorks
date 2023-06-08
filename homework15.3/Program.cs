829ab35f01a856056aa161056640ed7d7ea34970
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-06-08 12:43:39 +0400

    #changes

--- homework15.3/Program.cs
+++ homework15.3/Program.cs
@@ -1,11 +1,12 @@
 ﻿// ЗАДАЧА:
 // Создать проект консольной программы с решениями задач:
-//     ● Исходная последовательность содержит сведения об абитуриентах. Каждый элемент
+// ● Исходная последовательность содержит сведения об абитуриентах. Каждый элемент
 // последовательности включает следующие поля:
-//     <Фамилия> <Год поступления> <Номер школы>
-//     Для каждой школы вывести общее число абитуриентов за все годы и фамилию первого из абитуриентов
+// <Фамилия> <Год поступления> <Номер школы>
+//
+// Для каждой школы вывести общее число абитуриентов за все годы и фамилию первого из абитуриентов
 // этой школы, содержащихся в исходном наборе данных (вначале указывать номер школы, затем число
-//     абитуриентов, затем фамилию). Сведения о каждой школе выводить на новой строке и упорядочивать
+// абитуриентов, затем фамилию). Сведения о каждой школе выводить на новой строке и упорядочивать
 // по возрастанию номеров школ.
 
 using System;
@@ -13,6 +14,7 @@ using System.Reflection.Emit;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
+using System.Xml.Schema;
 
 namespace homework15._4
 {
@@ -22,33 +24,41 @@ namespace homework15._4
         {
             Console.WriteLine("Задача 3");
 
-            List<Enrollee> enrolleeList = new List<Enrollee>();
-
-            enrolleeList.Add(new Enrollee("Савельева Ульяна", 13, 2023));
-            enrolleeList.Add(new Enrollee("Павлов Ярослав", 123, 2022));
-            enrolleeList.Add(new Enrollee("Плотникова Ариана", 13, 2023));
-            enrolleeList.Add(new Enrollee("Кузнецов Павел", 13, 2021));
-            enrolleeList.Add(new Enrollee("Борисова Ева", 4, 2023));
-            enrolleeList.Add(new Enrollee("Кожевникова Вера", 13, 2021));
-            enrolleeList.Add(new Enrollee("Филатова Полина", 123, 2023));
-            enrolleeList.Add(new Enrollee("Денисова Татьяна", 18, 2022));
-            enrolleeList.Add(new Enrollee("Большаков Владислава", 23, 2022));
-            enrolleeList.Add(new Enrollee("Тарасова Арина", 3, 2023));
-            enrolleeList.Add(new Enrollee("Моисеев Михаил", 23, 2021));
-            enrolleeList.Add(new Enrollee("Литвинов Алексей", 18, 2021));
+            List<Enrollee> enrolleeList = new List<Enrollee>()
+            {
+                new Enrollee("Савельева Ульяна", 13, 2023),
+                new Enrollee("Павлов Ярослав", 123, 2022),
+                new Enrollee("Плотникова Ариана", 13, 2023),
+                new Enrollee("Кузнецов Павел", 13, 2021),
+                new Enrollee("Кузнецов Павел", 13, 2021),
+                new Enrollee("Борисова Ева", 4, 2023),
+                new Enrollee("Кожевникова Вера", 13, 2021),
+                new Enrollee("Филатова Полина", 123, 2023),
+                new Enrollee("Денисова Татьяна", 18, 2022),
+                new Enrollee("Большаков Владислава", 23, 2022),
+                new Enrollee("Тарасова Арина", 3, 2023),
+                new Enrollee("Моисеев Михаил", 23, 2021),
+                new Enrollee("Литвинов Алексей", 18, 2021)
+            };
 
             Console.WriteLine("* Исходная последовательность из учеников : \n" +
                               (String.Join("\n", enrolleeList)));
             Console.WriteLine(new string('-', 60));
 
             var subSequence = enrolleeList
+                .Where(x => true)
                 .GroupBy(x => x.SchoolNumber)
-                .ToList();
-            
+                .ToDictionary(
+                    x => x.Key,
+                    y => new
+                    {
+                        count = y.Count(),
+                        fullName = y.First().Name
+                    }
+                );
 
             Console.WriteLine("* Последовательность из учеников : \n" +
                               (String.Join("\n", subSequence)));
-
 
             //Задержка
             Console.ReadKey();
