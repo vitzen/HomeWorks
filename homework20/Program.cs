6ae0bf9a64aeef5da72638db85215ddcf59aeeaa
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-07-18 13:08:20 +0400

    #create homework20

---
+++ homework20/Program.cs
@@ -0,0 +1,29 @@
+﻿// ТЕКСТ ЗАДАНИЯ
+// Написать консольную программу, которая не блокируя основной поток опрашивает сайты
+// "http://yandex.ru",
+// "http://google.ru",
+// "http://vk.com",
+// "http://msdn.com",
+// c помощью WebClient.DownloadStringTaskAsync и возвращает на экран самый быстропришедший ответ.
+// Остальные запросы нужно отменять через CancelationToken. Используя Task и async/await
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Threading;
+
+namespace homework20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+        }
+    }
+}
