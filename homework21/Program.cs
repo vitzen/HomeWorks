c7449960e4da38aa643c5a5cbaa73250949e25df
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-07-21 15:26:18 +0400

    #create homework21

---
+++ homework21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+
+namespace homework20
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var drives = DriveInfo.GetDrives();
+            Console.WriteLine(drives);
+        }
+    }
+}
