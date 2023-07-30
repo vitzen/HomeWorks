284e34fbbca3731008134842bff3b7245ebc6289
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-07-30 18:46:02 +0400

    #changes

--- homework20/Program.cs
+++ homework20/Program.cs
@@ -37,13 +37,12 @@ namespace homework20
                 "http://msdn.com"
             };
 
-            var resultString = "";
             
             async Task<string> LoadAllDataByTasks()
             {
                 StringBuilder sb = new StringBuilder();
                 var time = Stopwatch.StartNew();
-            
+
                 foreach (var url in _url)
                 {
                     if (_cancellationTokenSource.IsCancellationRequested)
@@ -51,43 +50,41 @@ namespace homework20
                         sb.AppendLine("Была запрошена отмена операции.");
                         break;
                     }
-            
-                    resultString = await LoadDataAsync(url);
-                    sb.AppendLine($"{url}: {resultString}");
+
+                    var result = await LoadDataAsync(url);
+                    sb.AppendLine($"{url}: {result}");
                 }
-            
+
                 time.Stop();
-            
+
                 sb.AppendLine($"TOTAL time: {time.ElapsedMilliseconds} ms");
                 return sb.ToString();
             }
-            
-            //Отмена всех тасок
-            _cancellationTokenSource.Cancel();
-            _cancellationTokenSource.Dispose();
-            
-            
+
+
             async Task<string> LoadDataAsync(string url)
             {
                 var time = Stopwatch.StartNew();
                 var webClient = new WebClient();
                 try
                 {
-                    resultString = await webClient.DownloadStringTaskAsync(url);
+                    var result = await webClient.DownloadStringTaskAsync(url);
                     time.Stop();
-                    return $"SIZE:{resultString.Length}. TIME:{time.ElapsedMilliseconds} ms";
+                    return $"SIZE:{result.Length}. TIME:{time.ElapsedMilliseconds} ms";
                 }
                 catch (WebException e)
                 {
                     return $"Error load {url}. {e.Message}";
                 }
             }
-            
-            var a = LoadAllDataByTasks();
-            Console.WriteLine(String.Join(",", a));
-            Console.ReadKey();
 
-            
+            var a = LoadAllDataByTasks().ToString();
+            // Console.WriteLine(String.Join(",", a));
+            // Console.ReadKey();
+
+            //Отмена всех тасок
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
         }
     }
 }
