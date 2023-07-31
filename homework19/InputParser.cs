2ac7897c874032836c3ccb1545d803faafc3fd90
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-07-31 12:41:07 +0400

    #changes

--- homework19/InputParser.cs
+++ homework19/InputParser.cs
@@ -1,35 +1,38 @@
 namespace homework19;
 
-public class InputParser
+public static class InputParser
 {
-   
+    public static int cancelationTokenStatus()
+    {
+        do
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Для отмены  одной из Task's нажмите 1 \n" +
+                              "Для отмены всех Task's нажмите 2 \n");
+            Console.ForegroundColor = ConsoleColor.White;
 
+            var tokenStatus = false;
 
+            string inputSymbol = Console.ReadLine();
+            bool input = Int32.TryParse(inputSymbol, out int outputElement);
+            if (!input)
+            {
+                break;
+            }
+            else
+            {
+                switch (outputElement)
+                {
+                    case 1:
+                        tokenStatus = token.IsCancellationRequested == true;
+                        break;
+                    case 2:
+                        tokenStatus = token.IsCancellationRequested == true;
+                        break;
+                    default: break;
+                }
+            }
+        } while (inputFromUser == false);
+        return CancellationToken
+    }
 }
-
-
-// do
-// {
-//     Console.ForegroundColor = ConsoleColor.Yellow;
-//     Console.WriteLine("Для отмены  одной из Task's нажмите 1 \n" +
-//                       "Для отмены всех Task's нажмите 2 \n");
-//     Console.ForegroundColor = ConsoleColor.White;
-//
-//     string inputSymbol = Console.ReadLine();
-//     bool input = Int32.TryParse(inputSymbol, out int outputElement);
-//     if (!input)
-//     {
-//         break;
-//     }
-//     else
-//     {
-//         switch (outputElement)
-//         {
-//             case 1: break;
-//             case 2: break;
-//             default: break;
-//         }
-//     }
-// } while (inputFromUser == false);
-//
-// }
