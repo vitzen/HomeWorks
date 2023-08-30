47c4153bebac32d845afe02dedfda58ed0add922
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-08-30 16:23:43 +0400

    #created method CreateFile

--- homework21/Program.cs
+++ homework21/Program.cs
@@ -25,45 +25,59 @@ namespace homework21
             var drive = drives.First();
             DateTime dateTime = new DateTime();
             
-            var collection = new List<string?>();
+            var collection = new List<string?>()
+            {
+                
+            };
             collection.Add(Enumerable.Range(1, 100).ToString()); // Массив для записи в файл 1
             collection.Add(dateTime.ToString()); //Date Time для записи в файл 2
             collection.Add(Directory.GetDirectories(drive.RootDirectory.FullName).ToString()); //Список всех директорий для записи в файл 3
             
-
+            var directories = Directory.GetDirectories(drive.RootDirectory.FullName);
+            var currentPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             
-            //
-            // var directories = Directory.GetDirectories(drive.RootDirectory.FullName);
-            // var currentPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            //
-            // //Создаем дириекторию в каталоге проекта
-            // try
-            // {
-            //     var info = new DirectoryInfo(currentPath);
-            //     var createdDirectory = info.CreateSubdirectory("MyDirectory");
-            //
-            //
-            //     if (createdDirectory.Exists)
-            //     {
-            //         Console.WriteLine($"Папка |{createdDirectory.Name}| успешно создана по пути: {currentPath} ");
-            //     }
-            //
-            //     File.Create(Path.Combine(createdDirectory.FullName, "file1.txt"));
-            //
-            // }
-            // catch (Exception e)
-            // {
-            //     throw new Exception("Не удалось создать директорию по указанному пути");
-            // }
-            //
-            //
-            // FileStream CreateFile(string name)
-            // {
-            //     var fileInfo = new FileInfo(name);
-            //     if (!fileInfo.Exists)
-            //         return fileInfo.Create();
-            //     return File.Open(name, FileMode.Open);
-            // }
+            
+            //1. Создаем дириекторию в каталоге проекта
+            try
+            {
+                var info = new DirectoryInfo(currentPath);
+                var createdDirectory = info.CreateSubdirectory("MyDirectory");
+
+
+                if (createdDirectory.Exists)
+                {
+                    Console.WriteLine($"Папка |{createdDirectory.Name}| успешно создана по пути: {currentPath} ");
+                }
+
+                File.Create(Path.Combine(createdDirectory.FullName, "file1.txt"));
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Не удалось создать директорию по указанному пути");
+            }
+            finally
+            {
+                
+            }
+            
+            //2. Создаем в созданной нами дириектории 3 файла
+            
+            
+            FileStream CreateFile(string name)
+            {
+                var fileInfo = new FileInfo(name);
+                if (!fileInfo.Exists)
+                    return fileInfo.Create();
+                return File.Open(name, FileMode.Open);
+            }
+            
+            
+           
+            
+            
+            
+            
         }
     }
 }
